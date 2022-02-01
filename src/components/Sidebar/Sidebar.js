fc9c25797e5af260341bf103bdfb63d14663f574
Axel Olivas <axelbrolivas@gmail.com>
2022-02-01 13:00:20 -0800

    misc

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,9 +1,66 @@
 import PropTypes from 'prop-types'
+import {useState} from 'react'
+import axios from 'axios'
 import {NavLink, Link} from 'react-router-dom'
 import { HomeOutlined, Search, LibraryMusicOutlined, AddBox, FavoriteBorder } from '@mui/icons-material';
 import './sidebar.css'
 
-const Sidebar = ({className}) => {
+const PLAYLIST_ENDPOINT = 'https://api.spotify.com/v1/me/playlists'
+
+const Sidebar = ({className, token}) => {
+   console.log(token)
+   const DemoSidebar = () => {
+      return (
+         <div className='sidePlaylists'>
+            <Link to={{
+               pathname: '/playlist',
+               state: {title: 'Playlist #1'},
+            }} className='playlists'> Playlist #1
+            </Link>
+
+            <Link to={{
+               pathname: '/playlist',
+               state: {title: 'RapCaviar'},
+            }} className='playlists'>RapCaviar</Link>
+            <Link to={{
+               pathname: '/playlist',
+               state: {title: 'Liked from Radio'},
+            }} className='playlists'>Liked from Radio</Link>
+            <Link to={{
+               pathname: '/playlist',
+               state: {title: 'Discover Weekly'},
+            }} className='playlists'>Discover Weekly</Link>
+         </div>
+      )
+   }
+
+   const UserSidebar = (token) => {
+      const [data, setData] = useState({})
+
+      const handleGetPlaylists = async () => {
+         return axios.get(PLAYLIST_ENDPOINT, {
+            headers: {
+               Accept: 'application/json',
+               Authorization: 'Bearer ' + token,
+               'Content-Type': 'application/json',
+            },
+         }).then((res) => {
+            setData(res.data)
+            console.log(data)
+         }).catch((err) => {
+            console.log(err)
+         })
+      }
+
+      handleGetPlaylists()
+
+      return (
+         <>
+            {data?.items ? data.items.map((item) => <a href={item.href}>{item.name}</a>) : null}
+         </>
+      )
+   }
+
    return (
       <div className={className}>
          <div className='sideGroup'>
@@ -49,26 +106,7 @@ const Sidebar = ({className}) => {
             Liked Songs
          </Link>
          <hr/>
-         <div className='sidePlaylists'>
-            <Link to={{
-               pathname: '/playlist',
-               state: {title: 'Playlist #1'},
-            }} className='playlists'> Playlist #1
-            </Link>
-
-            <Link to={{
-               pathname: '/playlist',
-               state: {title: 'RapCaviar'},
-            }} className='playlists'>RapCaviar</Link>
-            <Link to={{
-               pathname: '/playlist',
-               state: {title: 'Liked from Radio'},
-            }} className='playlists'>Liked from Radio</Link>
-            <Link to={{
-               pathname: '/playlist',
-               state: {title: 'Discover Weekly'},
-            }} className='playlists'>Discover Weekly</Link>
-         </div>
+         {token ? <UserSidebar /> : <DemoSidebar />}
       </div>
    )
 }
